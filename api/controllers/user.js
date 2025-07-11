7a2e0a853e35181749d93a85675de254a03ca51e
Lucca <cerasommalucca@gmail.com>
2025-07-11 08:17:36 -0300

    finalizando a requisicao get

---
+++ api/controllers/user.js
@@ -0,0 +1,12 @@
+import { db } from "../db.js";
+
+//passei a requisicao como "_" pois ja que é uma get nao precisa do req apenas ler entao so passo o res
+export const getUsers = (_,res) =>{
+    const q = "SELECT * FROM usuarios";
+
+    db.query(q, (err, data)=>{
+        if (err) return res.json(err)
+         
+        return res.status(200).json(data)
+    })
+}
